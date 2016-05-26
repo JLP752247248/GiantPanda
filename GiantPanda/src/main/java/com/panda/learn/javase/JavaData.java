f155fcd00bb87dbacfae30443960ff4414cb022b
jlp752247248 <752247248@QQ.COM>
2016-05-26 13:16:50 +0800

    5

--- GiantPanda/src/main/java/com/panda/learn/javase/JavaData.java
+++ GiantPanda/src/main/java/com/panda/learn/javase/JavaData.java
@@ -12,9 +12,9 @@ public class JavaData {
 		int _int=-2147483648;
 		byte _byte=-128;//8位，一共可表示2^8个数，正负除以二，就是2^7=128个，空出以为来表示符号，再从正数里找一位表示0
 		
-		for(int i=-128;i<128;i++){
+		/*for(int i=-128;i<128;i++){
 			System.out.println(Integer.toBinaryString(i)+"  "+i);
-		}
-		//System.out.println(_byte);
+		}*/
+		System.out.println(0xffffffff);
 	}
 }
